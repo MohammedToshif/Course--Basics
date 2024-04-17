ffb6aa8a69d9324de264ab1f5a388faee8bdf294
MohammedToshif <toshif92612@gmail.com>
2024-04-17 15:12:51 +0530

    Course Basics

---
+++ htdocs/main.js
@@ -0,0 +1,50 @@
+
+
+$(window).on("load", function() {
+    
+    // preloader
+    $(".preloader").fadeOut (2000);
+    // home section slideshow
+    let slideIndex = $(".slide.active").index();
+    const slideLen = $(".slide").length;
+    
+    function slideShow() {
+        $(".slide").removeClass("active").eq(slideIndex).addClass("active");
+        if(slideIndex == slideLen-1) {
+            slideIndex = 0;
+        }
+        else {
+            slideIndex++;
+        }
+        setTimeout(slideShow,5000)
+    }
+    slideShow();
+})  
+
+// nav toggle
+
+$(".hamburger-btn").click(function() {
+    $(".header .nav").slideToggle();
+})
+$(".header .nav a").click(function() {
+    if($(window).width() < 768) {
+        $(".header .nav").slideToggle();
+    }
+})
+
+// fixed header
+
+$(window).scroll(function() {
+    if($(this).scrollTop() > 100) {
+        $(".header").addClass("fixed");
+    }
+    else {
+        $(".header").removeClass("fixed");
+    }
+})
+
+// scrollIt
+
+$.scrollIt({
+    topOffset: -50
+});
